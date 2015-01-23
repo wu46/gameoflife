22f0e6edb599be1211bcfed319619dd2bccdd1a1
Jennifer Wu <jenniferwu46@gmail.com>
2015-01-22 23:07:15 -0800

    Added a center button

--- src/lifegui.cpp
+++ src/lifegui.cpp
@@ -3,32 +3,39 @@
 
 LifeGUI::LifeGUI()
 {
-  gw.setSize(defaultWidth, defaultHeight);
+  //gw = GWindow(defaultWidth, defaultHeight);
+  cout << "made gwindow of size" << gw.getCanvasSize() << endl;
   gw.requestFocus();
   originX = (int) gw.getWidth() * marginRatio;
   originY = (int) gw.getHeight() * marginRatio;
+  cellOffsetX = 0;
+  cellOffsetY = 0;
 
   // add interactors
   tickButton = new GButton("tick");
+  pauseButton = new GButton("pause");
   animateButton = new GButton("animate");
   quitButton = new GButton("quit");
   animateTextField = new GTextField(5);
   animateTextField->setText("50");
+  centerButton = new GButton("center");
 
   gw.addToRegion(tickButton, "SOUTH");
   gw.addToRegion(animateButton, "SOUTH");
   gw.addToRegion(animateTextField, "SOUTH");
   gw.addToRegion(quitButton, "SOUTH");
+  gw.addToRegion(centerButton, "EAST");
 
 }
 
 LifeGUI::~LifeGUI()
 {
-  //gw.close();
+  gw.close();
 }
 
 void LifeGUI::run(){
   GameOfLife game("fishcoord.txt");
+  Set<PointLL> world;
   cout << "initialized game of life" << endl;
   while (true) {
     int it = 0;
@@ -43,57 +50,140 @@ void LifeGUI::run(){
     if (e.getActionCommand().compare("quit") == 0){
       return;
     }
+    if (e.getActionCommand().compare("center") == 0) {
+      center(world);
+      nIterations = 0;
+    }
 
     while (it < nIterations){
       game.tick();
-      Set<PointLL> world = game.getWorld();
+      world = game.getWorld();
       drawBoard(world);
 
-      pause(100);
+      pause(PAUSE_TIME);
       it++;
     }
   }
 }
 
-/*
-void LifeGUI::drawBoard(Grid<int> &world){
-  gw.clear();
-  for(int i=0; i<world.nRows; i++){
-      for (int j=0; j<world.nCols; j++){
-          drawCell(i,j, (world[i][j]!=0));
-      }
-  }
-}
-*/
-
 void LifeGUI::drawBoard(Set<PointLL> &world){
-  clearBoard(world);
+  refreshBoard(world);
   currentWorld = world;
   gw.setColor("BLACK");
   for (Set<PointLL>::iterator it = world.begin(); it != world.end(); it++)
-    {
-      drawCell((*it).getX(), (*it).getY());
-    }
+  {
+    drawCell((*it).getX(), (*it).getY());
+  }
   return;
 }
 
 void LifeGUI::clearBoard(Set<PointLL> &world){
   gw.setColor("WHITE");
-  for (Set<PointLL>::iterator it = currentWorld.begin(); it != currentWorld.end(); it++)
-    {
-      // if not contained in next world, erase it
-      if (!world.contains(*it))
-        {
-          drawCell((*it).getX(), (*it).getY());
-        }
-
-    }
+  for (Set<PointLL>::iterator it = world.begin(); it != world.end(); it++)
+  {
+      drawCell((*it).getX(), (*it).getY());
+  }
   return;
 }
 
+void LifeGUI::refreshBoard(Set<PointLL> &world){
+  gw.setColor("WHITE");
+  for (Set<PointLL>::iterator it = currentWorld.begin(); it != currentWorld.end(); it++)
+  {
+    // if not contained in next world, erase it
+    if (!world.contains(*it))
+    {
+      drawCell((*it).getX(), (*it).getY());
+    }
+
+  }
+  return;
+}
+
+void LifeGUI::center(Set<PointLL> &world) {
+  // check if world is initialized. if not, center does nothing
+  if (world.size()<1) {
+    return;
+  }
+  // clear current board
+  clearBoard(world);
+  /*
+  if (c == NULL){
+    PointLL * c = world.first();
+  }
+  */
+  PointLL c = world.first();
+
+  // draw new board
+
+  cout << "the point chosen as center is: " << c.toString() << endl;
+  cout << "origin at " << originX << "," << originY << endl;
+  cout << "width: " << gw.getWidth() << "height: " << gw.getHeight() << endl;
+  int xloc = originX + c.getX()*cellWidth + cellOffsetX;
+  int yloc = originY + c.getY()*cellWidth + cellOffsetY;
+  cout << "old xloc: " << xloc << " yloc: " << yloc << endl;
+  //originX = gw.getWidth()/2 - c.getX();
+  //originY = gw.getHeight()/2 - c.getY();
+  cellOffsetX = gw.getWidth()/2.0 - originX - c.getX()*cellWidth;
+  cellOffsetY = gw.getHeight()/2.0 - originY - c.getY()*cellWidth;
+
+  cout << "cellOffsetx=" << cellOffsetX << " celloffsety="
+       << cellOffsetY << endl;
+
+  xloc = originX + c.getX()*cellWidth + cellOffsetX;
+  yloc = originY + c.getY()*cellWidth + cellOffsetY;
+  cout << "new xloc: " << xloc << " yloc: " << yloc << endl;
+
+
+
+  drawBoard(world);
+}
+
+/*
+void LifeGUI::center(Set<PointLL> &world) {
+  // calculate a centroid
+  // simple k-means
+  Vector<Centroid> centroids;
+  Vector<Set<PointLL>> centroidChildren;
+
+
+  //add the first centroid
+  centroids.add(*world.begin());
+
+  // if everyone is within reasonable distance, everyone can be assigned to it
+  // not reasonable: delta x or delta y more than half max width or max height
+  int xDistanceCutoff = gw.getWidth()/2;
+  int yDistanceCutoff = gw.getHeight()/2;
+
+  for (Set<PointLL>::iterator it=world.begin();
+      it!=world.end();
+      it++) {
+    for (int i=0; i<centroids.size(); i++){
+      if (abs((*it).getX() - (*centroidIt).getX()) > xDistanceCutoff ||
+          abs((*it).getY() - (*centroidIt).getY()) > yDistanceCutoff) {
+        centroidIt++;
+      } else {
+        // TODO:!!!! finish this
+      }
+    }
+
+  }
+}
+*/
+
 void LifeGUI::drawCell(int x, int y){
-  GRectangle cell(originX + x * cellWidth,
-               originY + y * cellWidth,
-               cellWidth, cellWidth);
-  gw.fillRect(cell);
+  // if cell is out of bounds, then don't draw it
+  int xloc = originX + x*cellWidth + cellOffsetX;
+  int yloc = originY + y*cellWidth + cellOffsetY;
+
+  if (xloc < 0 || xloc > (int)gw.getWidth() ||
+      yloc < 0 ||yloc > (int)gw.getHeight()) {
+    // don't draw the cell
+  } else {
+    GRectangle cell(xloc, yloc, cellWidth, cellWidth);
+    gw.fillRect(cell);
+  }
+
+
+
 }
--- src/lifegui.h
+++ src/lifegui.h
@@ -19,22 +19,31 @@ public:
     void run();
 
 private:
-    GWindow gw;
+    GWindow gw = GWindow(defaultWidth, defaultHeight);
     Set<PointLL> currentWorld;
+
+    const static int PAUSE_TIME = 50;
     const static int defaultWidth = 1000;
     const static int defaultHeight = 500;
     const static int cellWidth = 5;
     // how much of window is used as margin
-    const float marginRatio = 0.2;
+    const float marginRatio = 0.1;
     int originX; //TODO: recompute these when resized
     int originY;
+    int cellOffsetX; // coord offset
+    int cellOffsetY;
+    //PointLL c = PointLL(0,0);
     void drawCell(int x, int y);
+    void refreshBoard(Set<PointLL> &world);
     void clearBoard(Set<PointLL> &world);
+    void center(Set<PointLL> &world);
 
     // GUI components
     GButton *tickButton;
     GButton *animateButton;
     GButton *quitButton;
+    GButton *centerButton;
+    GButton *pauseButton;
     GTextField *animateTextField;
 };
 
